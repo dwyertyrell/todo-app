6b08bdb91a49b2f927bf01a6423d39aab664e410
dwyertyrell <Dwyertyrell@gmail.com>
2025-09-01 21:17:58 +0100

    Update text.jsx

--- frontend/src/components/atomic/text.jsx
+++ frontend/src/components/atomic/text.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-const Text = ({children, color='inherit' ,size= 'md', ...props})=> { 
+const Text = ({children, color='inherit' , size= 'md', ...props})=> { 
   //dynamic styling (that relies on the state of the parent prop) needs to be declared inside the component
   const styles = {
   fontSize: size === 'sm' ? '0.875rem' : size === 'lg'? '1.25rem': '1rem',
