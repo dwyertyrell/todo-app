04eb076d2a74b12ca50800c342474f3e4faed77f
dwyertyrell <Dwyertyrell@gmail.com>
2025-09-04 13:31:32 +0100

    separated the bulk action routes, from the todo CRUD

--- backend/src/routes/todos.js
+++ backend/src/routes/todos.js
@@ -3,13 +3,19 @@ const router = express.Router();
 const todosController = require('../controllers/todosController');
 //defines which endpoint gets to exist and which controller handles them
 //the HTTP method coupled with the endpoint, is enough info to match each api url with its corresponding request handler
+
+// --- Todo CRUD --- 
 router.get('/', todosController.getTodosController); 
 router.post('/', todosController.createTodoController);
 router.put('/:id', todosController.updateTodoController)
 router.delete('/:id', todosController.deleteTodoController)
-router.delete('/', todosController.deleteAllTodoController)
 router.put('/:id/completed', todosController.toggleCompletedController)
+
+// --- Bulk Actions ---
 router.put('/', todosController.toggleAllCompletedController)
+router.delete('/', todosController.deleteAllTodoController)
+
+
 
 // attaching each HTTP route to the router object, then export to be accessed by the controller dir for validation 
 
