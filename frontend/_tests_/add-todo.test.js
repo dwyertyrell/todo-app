df8666e91221139b2e2d1573b61ece96112894a6
dwyertyrell <Dwyertyrell@gmail.com>
2025-09-08 12:28:49 +0100

    (refactor) frontend tests: Add Jest testing setup and todo form tests
    Removed global test folder (`./_tests_`) from root directory and placed testing inside the frontend.  Client-side can only be tested from inside the frontend directory
    
    Introduces Jest configuration and Babel setup for frontend testing.
    
    Adds tests for AddTodoForm validation on the client side- testing on the react elements .
    
    Updates ESLint globals in `eslint.config.js` to include Jest and adds related testing dependencies to `frontend/package.json`.

---
+++ frontend/_tests_/add-todo.test.js
@@ -0,0 +1,48 @@
+import React from "react";
+import '@testing-library/jest-dom'
+import AddTodoForm from "../src/components/molecules/addTodoForm";
+import userEvent from "@testing-library/user-event";
+import { render } from "@testing-library/react";
+
+describe('AddTodoForm client-side validation', () => {
+    
+    let onAdd;
+    let notify;
+    let addTodoForm
+    let input;
+    let button;
+
+  beforeEach(() => {
+    onAdd = jest.fn()
+    notify= jest.fn()
+    addTodoForm = render(<AddTodoForm onAdd={onAdd} notify={notify} todos={[]}/>)
+    input = addTodoForm.queryByRole('textbox')
+    button = addTodoForm.queryByRole('button')
+  })
+
+  test('should add valid todo', async () => {
+    await userEvent.type(input, 'walk dog home')
+    await userEvent.click(button)
+    // addTodoForm.debug()
+    expect(onAdd).toHaveBeenCalledWith('walk dog home')
+    expect(notify).toHaveBeenCalledWith('todo item added to list!', 'success')
+  })
+
+  test('should not add empty todo', async () => {
+    addTodoForm.rerender(<AddTodoForm onAdd={onAdd} notify={notify} todos={[{text: 'buy milk'}]}/>)
+
+    await userEvent.type(input, ' ')
+    await userEvent.click(button)
+    expect(onAdd).not.toHaveBeenCalledWith(' ')
+    expect(notify).toHaveBeenCalledWith('todo item cannot be empty', 'error')
+  })
+
+  test('should not add a duplicated todo', async() => {
+    addTodoForm.rerender(<AddTodoForm onAdd={onAdd} notify={notify} todos={[{text: 'buy milk'}]}/>)
+
+    await userEvent.type(input, 'buy milk')
+    await userEvent.click(button)
+    expect(onAdd).not.toHaveBeenCalledWith('buy milk')
+    expect(notify).toHaveBeenCalledWith('duplicated todo items are not allowed', 'error')
+  })
+})
