4ca83b9ab9f2b84ebe56c7c040e97df6ca441789
dwyertyrell <Dwyertyrell@gmail.com>
2025-09-14 17:15:27 +0100

    removed commented code

--- frontend/src/components/organisms/todoList.jsx
+++ frontend/src/components/organisms/todoList.jsx
@@ -2,11 +2,10 @@ import React from 'react'
 import TodoItem from './todoItem'
 
 function TodoList ({todos, onUpdate, onDelete, onToggleCompleted, notify}) {
-  // const safeTodos = Array.isArray(todos) ? todos : []
   if(todos.length === 0) { return <p>there are no todo items yet.</p>}
   return (
       <ul>
-        {/*safeTodos*/todos.map((todo)=> {
+        {todos.map((todo)=> {
           return (
             <TodoItem
               key={todo.id}
