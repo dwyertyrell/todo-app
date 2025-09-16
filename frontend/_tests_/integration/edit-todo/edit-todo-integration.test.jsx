3c5ddc58ef23b50668b8a7ba1ec42a9fa18f294c
dwyertyrell <Dwyertyrell@gmail.com>
2025-09-16 16:17:56 +0100

    tests: Edit-todo testing on client-side
    added user story in `edit-todo.feature` and created test cases on these scenarios in the `edit-todo-integration.test.jsx` file.
    
    `edit-todo-integration.test.jsx`:
    
    Additional callback functions was added in order to manage the piece of state in the test environment. `line 17-30`
    
    In order to complete the test case for a bulk update(`line 134-150`), a refactor was needed to the todo list in the piece of state: to hold more than one todo item `todos` on `line 13`.
    
    on `line 95-101`, user event  added to close the notification component after the test case.
    
    additional test cases were created (`line 104-130`) for validating the edit button and checkbox functionality, on each todo item.

--- frontend/_tests_/integration/edit-todo/edit-todo-integration.test.jsx
+++ frontend/_tests_/integration/edit-todo/edit-todo-integration.test.jsx
@@ -5,39 +5,56 @@ import userEvent from "@testing-library/user-event";
 
 import Notification from "../../../src/components/atomic/notification/notification";
 import TodoList from "../../../src/components/organisms/todoList";
+import BulkActions from "../../../src/components/molecules/bulkActions";
 
-//simulating a stateful environment for testing
+//Simulating a stateful environment for testing
 function TestWrapper() {
 
-  const [todos, setTodos] = useState([{ id: 1, text: 'wash the bath', completed: false, }])
-  const [notifyMessage, setNotifyMessage] = useState('')
-  const [notifyType, setNotifyType] = useState('')
+  const [todos, setTodos] = useState([{ id: 1, text: 'wash the bath', completed: false, }, { id: 2, text: 'watch anime', completed: false, }])
+  const [notifyMessage, setNotifyMessage] = useState(' ')
+  const [notifyType, setNotifyType] = useState(' ')
 
+  const handleComplete = (id) => {
+    setTodos(
+      todos.map(todo => todo.id === id ? !todo.completed : todo.completed )
+    )
+  }
+
+  const handleCompleteAll = () => {
+    setTodos((todos) => {
+      return (
+      todos.map(todo => !todo.completed ? {...todo, completed: true} : {...todo})
+      )
+    })
+  
+    }
 
   const handleUpdate = (id, newText) => {
     setTodos(todos => {
       return (
         todos.map(todo => todo.id === id ? {...todo, text: newText} : todo)
-      )
-    })
+      )})
     }
+
   const handleNotify = (msg, type) => {
     setNotifyMessage(msg)
     setNotifyType(type)
     }
+
   const onClose = () => {
     setNotifyMessage('')
     setNotifyType('')
   }
+
   return (
     <>
-      <TodoList todos={todos} onUpdate={handleUpdate} onDelete={() => {}} onToggleCompleted={() => {}} notify= {handleNotify}/>
+      <BulkActions deleteAllTodos={()=>{}} completeAllTodos={handleCompleteAll} />
 
+      <TodoList todos={todos} onUpdate={handleUpdate} onDelete={() => {}} onToggleCompleted={handleComplete} notify= {handleNotify}/>
       <Notification message={notifyMessage} type={notifyType} onClose={onClose} />
     </>
   )
 }
-
 
 describe('Editing a todo- client side validation', () => {
 
@@ -48,15 +65,14 @@ describe('Editing a todo- client side validation', () => {
   })
 
   test('user edits and updates a todo item in list', async() => {
-    
-    const editButton = view.queryByRole('button', {name: 'Edit'})
+    const todoItem = view.container.querySelector('[data-testid="1"]')
+    const editButton = within(todoItem).queryByRole('button', {name: 'Edit'})
     await userEvent.click(editButton)
-    const item = view.container.querySelector('.todoItem')
-    const textbox = within(item).getByDisplayValue('wash the bath') 
+    const textbox = within(todoItem).getByDisplayValue('wash the bath') 
     expect(textbox).toBeInTheDocument()
     await userEvent.clear(textbox)
     await userEvent.type(textbox, 'go to sleep')
-    const saveButton = view.queryByRole('button', {name: 'Save'})
+    const saveButton = within(todoItem).queryByRole('button', {name: 'Save'})
     await userEvent.click(saveButton)
 
     const confirmModal = view.container.querySelector('.modal-overlay')
@@ -76,12 +92,68 @@ describe('Editing a todo- client side validation', () => {
     await waitFor(() => {
     expect(notification).toBeInTheDocument()
     })
+    // use the afterEach() test function to render the <Notification/> 's props with empty string values
+    const closeButton = within(notification).getByRole('button')
+    await userEvent.click(closeButton)
+    await waitFor(() => {
+      expect(notification).not.toBeInTheDocument()
+    })
   })
+
+//did i test the notification modal in this case?
+  test('User cancels editing; the todo returns to its previous state', async() => {
+    const todoItem = view.container.querySelector('[data-testid="1"]')
+    const editButton = within(todoItem).queryByRole('button', {name: 'Edit'})
+    await userEvent.click(editButton)
+    const textbox = within(todoItem).getByRole('textbox')
+    expect(textbox).toBeInTheDocument()
+    const cancelButton = within(todoItem).getByRole('button', {name: 'Cancel'})
+    expect(cancelButton).toBeInTheDocument()
+    await userEvent.click(cancelButton)
+    await waitFor(() => {
+      expect(textbox).not.toBeInTheDocument()
+    })
+  })
+
+  /*  needed to replace dummy callback in the onToggleCompleted prop
+  with a real handler that manages a piece of state, to allow for state change in the component; 
+  simulating the user click event
+  */ 
+
+  test('User marks a todo item as "complete"', async() => {
+    const todoItem = view.container.querySelector('[data-testid="2"]')
+    const checkbox = within(todoItem).getByRole('checkbox')
+    expect(checkbox).toBeInTheDocument()
+    await userEvent.click(checkbox)
+    await waitFor(() => {
+      expect(checkbox).toBeChecked()
+    })
+  })
+
+  // check test- falsey positive 
+  test('User marks all todo items as completed', async() => {
+    const completeAllButton = view.getByRole('button', {name: 'Complete All'})
+    expect(completeAllButton).toBeInTheDocument()
+    await userEvent.click(completeAllButton)
+    const confirmButton = view.getByRole('button', {name: 'Confirm'})
+    expect(confirmButton).toBeInTheDocument()
+    await userEvent.click(confirmButton)
+    const todoItem1 = view.container.querySelector('[data-testid="1"]')
+    const todoItem2 = view.container.querySelector('[data-testid="2"]')
+    const checkbox1 = within(todoItem1).getByRole('checkbox')
+    const checkbox2 = within(todoItem2).getByRole('checkbox')
+    expect(checkbox1).toBeInTheDocument()
+    expect(checkbox2).toBeInTheDocument()
+    await waitFor(() => {
+      expect(checkbox1).toBeChecked()
+      expect(checkbox2).toBeChecked()
+    })
+  })
+
+  
 })
 
-// describe('User cancels editing; the todo returns to its previous state', () => {
 
-// })
 
 
 
--- frontend/_tests_/integration/edit-todo/edit-todo.feature
+++ frontend/_tests_/integration/edit-todo/edit-todo.feature
@@ -1,8 +1,5 @@
 Feature: Editing a todo 
 
-
-
-# create a 2nd integration test file for this, or a describe()?
 As a user, i want to edit an existing todo so i can update my tasks
 
 Scenario: User edits and update a todo item in the list 
@@ -18,3 +15,17 @@ Scenario: User cancels editing; the todo returns to its previous state
   Given Editing mode is enabled
   When the user clicks the 'Cancel' button
   Then the editing mode is disabled; the 'Edit' button is appears next to the todo item.
+
+As a user, i want to mark a todo as completed so i can see which tasks are done.
+
+  Scenario: User marks a todo item as 'complete'
+    Given the todo list is rendered with the checkbox
+    When the user selects the checkbox 
+    Then a strike-through is shown on the todo text
+
+As a user, i want to mark all todo items as completed, at once.
+
+  Scenario: User marks all todo items as completed
+    Given the todo list is rendered
+    When the user clicks the 'Complete All' button
+    Then every todo item is marked as completed   
